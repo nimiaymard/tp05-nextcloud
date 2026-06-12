23053322b6daf83f0fe5de2f96a5c73e3b86bfae
Amsa36 <amsatou36@gmail.com>
2026-06-12 14:45:21 +0200

    fix: use networking module in dev environment

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -1,8 +1,15 @@
 module "networking" {
-  source       = "../../modules/vpc"
+  source       = "../../modules/networking"
   project_name = var.project_name
   environment  = var.environment
+  vpc_cidr     = "10.30.0.0/16"
+  azs          = ["eu-west-3a", "eu-west-3b"]
 }
 
-output "vpc_id" { value = module.networking.vpc_id }
-output "subnet_id" { value = module.networking.public_subnet_id }
+output "vpc_id" {
+  value = module.networking.vpc_id
+}
+
+output "public_subnet_ids" {
+  value = module.networking.public_subnet_ids
+}
